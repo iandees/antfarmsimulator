45bcbe07a336278fa1d5ad4aa5a1a97016d45f3d
ian.dees <ian.dees@03954fbd-d825-0410-bf72-996cafae8b4f>
2006-12-30 21:26:15 +0000

    Initial import.

---
+++ AntfarmSimulator/src/com/mapki/antfarm/creatures/Ant.java
@@ -0,0 +1,16 @@
+package com.mapki.antfarm.creatures;
+
+import javax.vecmath.Vector3d;
+
+public class Ant {
+    /** The ant's location in 3D space. */
+    private Vector3d location;
+    
+    /** The ant's current speed and direction. */
+    private Vector3d velocity;
+    
+    /** The ant's health points. */
+    private int health;
+    
+    
+}
